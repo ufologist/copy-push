2fccda2cfc1e8832ee6dab39aae0d62ecd8e9523
ufologist <klsforever@gmail.com>
2020-03-04 14:52:54 +0800

    feat: 在 copy-push 默认的提交信息中增加远端项目名, 当前分支名和最近一次的提交日志

--- src/defaults.js
+++ src/defaults.js
@@ -1,9 +1,62 @@
+var childProcess = require('child_process');
+
+/**
+ * 获取 Git 最近一次的提交日志
+ * 
+ * @return {string}
+ */
+function getLatestGitLog() {
+    var log = '';
+    try {
+        log = childProcess.execSync('git log -1 --pretty=format:"%h %cn %cd" --date=format:"%Y-%m-%d %H:%M:%S"').toString();
+        // 去除最后的换行字符
+        log = log.substring(0, log.length - 1);
+    } catch (error) {
+        console.warn('getLatestGitLog error', error.message);
+    }
+    return log;
+}
+
+/**
+ * 获取 Git 远端项目名
+ * 
+ * @return {string}
+ */
+function getRemoteRepoName() {
+    var remoteRepoName = '';
+    try {
+        // 获取 origin 远端的URL, 例如 https://github.com/ufologist/copy-push.git
+        remoteRepoName = childProcess.execSync('git remote get-url origin --push').toString();
+        remoteRepoName = remoteRepoName.substring(remoteRepoName.lastIndexOf('/') + 1, remoteRepoName.length - 1);
+    } catch (error) {
+        console.warn('getRemoteRepoName error', error.message);
+    }
+    return remoteRepoName;
+}
+
+/**
+ * 获取 Git 当前分支名
+ * 
+ * @return {string}
+ */
+function getBranchName() {
+    var branchName = '';
+    try {
+        branchName = childProcess.execSync('git symbolic-ref --short HEAD').toString();
+        branchName = branchName.substring(0, branchName.length - 1);
+    } catch (error) {
+        console.warn('getBranchName error', error.message);
+    }
+    return branchName;
+}
+
 var src = './dist/**/*.html';
+var message = 'sync [' + src + '] ' + getRemoteRepoName() + ':' + getBranchName() + ' ' + getLatestGitLog();
 
 module.exports = {
     src: src,
     destDir: 'src/main/webapp/views',
     remote: 'origin',
     branch: 'master',
-    message: 'sync [' + src + ']'
+    message: message
 };
